20c82924a7edddcbf5cd63b3b66fc96075ac0674
Ivan <xefzar@gmail.com>
2018-05-25 00:00:15 +0300

    Added base structure

---
+++ cb.core/src/cb/core/editors/CodeBuilderMultiPageEditor.java
@@ -0,0 +1,52 @@
+package cb.core.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.MultiPageEditor;
+
+//TODO change superclass
+public class CodeBuilderMultiPageEditor extends MultiPageEditor {
+
+  public CodeBuilderMultiPageEditor() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void doSave(IProgressMonitor monitor) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void doSaveAs() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean isDirty() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean isSaveAsAllowed() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void setFocus() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
