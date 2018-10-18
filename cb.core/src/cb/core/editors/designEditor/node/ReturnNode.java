af8c5a2676340c847973d91c9c3aea6acd4ac92d
Ivan <xefzar@gmail.com>
2018-10-18 07:56:25 +0300

    deleted old files

--- cb.core/src/cb/core/editors/designEditor/node/ReturnNode.java
+++
@@ -1,9 +0,0 @@
-package cb.core.editors.designEditor.node;
-
-import java.util.HashMap;
-
-public class ReturnNode extends MethodNode{
-  public ReturnNode(String codeTemplate, HashMap<String, String> keywordValueMap) {
-    super(codeTemplate, keywordValueMap, MethodNode.RETURN);
-  }
-}
