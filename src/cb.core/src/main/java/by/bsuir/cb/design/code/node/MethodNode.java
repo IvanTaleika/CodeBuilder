141e808783de6c1735ad7b26a1dccd14d6024bfa
Ivan <xefzar@gmail.com>
2018-12-04 00:27:39 +0300

    Fixed plugin description

--- src/cb.core/src/main/java/by/bsuir/cb/design/code/node/MethodNode.java
+++ src/cb.core/src/main/java/by/bsuir/cb/design/code/node/MethodNode.java
@@ -3,16 +3,18 @@ package by.bsuir.cb.design.code.node;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import lombok.Data;
 
-
+// TODO refactor
 // TODO rename package
+@Data
 public abstract class MethodNode {
   public static final String CONDITION = "condition";
   public static final String FUNCTION = "function";
   public static final String RETURN = "return";
 
-  private final String type;
-  private final LinkedList<MethodNode> previousNodes;
+  private String type;
+  private LinkedList<MethodNode> previousNodes;
   private String codeTemplate;
   private HashMap<String, String> keywordValueMap;
 
@@ -34,18 +36,6 @@ public abstract class MethodNode {
   protected void removePrevious(MethodNode methodNode) {
     previousNodes.remove(methodNode);
 
-  }
-
-  public String getCodeTemplate() {
-    return codeTemplate;
-  }
-
-  public HashMap<String, String> getKeywordValueMap() {
-    return keywordValueMap;
-  }
-
-  public String getType() {
-    return type;
   }
 
 
